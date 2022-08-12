b05d3db9e309e5778d39ba6c361e5388e8a1ddbb
mbukeRepo <mbuke404@gmail.com>
2022-08-12 22:45:17 +0200

    configured map on my webpage

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import User from "./User";
 import Company from "./Company";
-
+// testing my classes
 const user = new User();
 const company = new Company();
-console.log(user.name);
-console.log(company.companyName, company.catchPhrase);
+// creating new map on a webpage
+const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
+console.log(map);

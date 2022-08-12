0370823cea0f92dada5ba699d5565fc3a6dfe66a
mbukeRepo <mbuke404@gmail.com>
2022-08-12 20:51:03 +0200

    initial commit

---
+++ src/User.ts
@@ -0,0 +1,15 @@
+import { faker } from '@faker-js/faker';
+class User {
+  name: String;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+     this.name = faker.name.firstName();
+     this.location = {
+       lat: parseFloat(faker.address.latitude()), 
+       lng: parseFloat(faker.address.longitude())
+     };
+  }
+}
